141b5c4f3f1df3ad19267322de65a0413f489656
jagmohan sharma <jagmohan.20223117@gmail.com>
2025-06-17 12:53:26 +0000

    db setup postgresql

---
+++ Designs.md
@@ -0,0 +1,8 @@
+# technologies
+
+**frontend** -: React
+**front to backend** -: RESI-APIs
+**backend** -: SpringBoot
+**Database** -: PostgreSQL
+**AuthServer** -: keycloak {connected to both frontend and backend}
+
---
+++ tickit/src/test/java/com/jaggi/tickit/TickitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaggi.tickit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TickitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
